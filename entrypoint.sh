ae319b839dcb633df75e1f81b5b5e1627b3a9218
Mathias Pius <contact@pius.io>
2022-01-30 09:43:09 +0100

    Explain what the entrypoint is doing, and dump the finished config file

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,11 @@ export PACKER_ANSIBLE_DIR=/cache/.ansible
 # Combine all the configuration paths passed as arguments.
 jq -s 'reduce .[] as $item ({}; . * $item)' "$@" > config.json
 
+echo "Combined configuration:"
+cat config.json
+
+
+echo "Starting Packer Build"
 /usr/bin/packer build          \
     -var-file="config.json"    \
     .
